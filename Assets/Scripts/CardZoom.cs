2ad3d0e85d13f638a684762e9ac81b010c24e706
IrinaChitu <chitu.irina.nicoleta@gmail.com>
2020-11-07 15:17:44 +0200

    Initial commit
    Added raw functionalities

---
+++ Assets/Scripts/CardZoom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardZoom : MonoBehaviour
+{
+    public GameObject Canvas;
+
+    private GameObject zoomCard;
+
+    public void Awake()
+    {
+        Canvas = GameObject.Find("Main Canvas");
+    }
+
+    public void OnHoverEnter()
+    {
+        // + 100 -> will only work for the lower row; could treat them separately -> instantiate them from drawCards as two different types so that we can particularize them here
+        // or choose an absolute value where to show them (eg: middle left)
+        zoomCard = Instantiate(gameObject, new Vector2(Input.mousePosition.x, Input.mousePosition.y + 120), Quaternion.identity);
+        zoomCard.transform.SetParent(Canvas.transform, false);
+        zoomCard.layer = LayerMask.NameToLayer("Zoom"); // alternative: remove the collider from the zoomCard
+
+        RectTransform rect = zoomCard.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(150, 210); // (3*rect.sizeDelta.x, 3*rect.sizeDelta.y)
+    }
+
+    public void OnHoverExit()
+    {
+        Destroy(zoomCard);
+    }
+}
---
+++ Assets/Scripts/DrawCard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawCard : MonoBehaviour
+{
+    public GameObject Card;
+    public GameObject PlayerArea;
+    public GameObject EnemyArea;
+
+    List<GameObject> cards = new List<GameObject>();
+    List<Color32> colors = new List<Color32>();
+
+    private void Start()
+    {
+        cards.Add(Card);
+        cards.Add(Card);
+        colors.Add(new Color32(32, 21, 236, 255));
+        colors.Add(new Color32(236, 21, 34, 255));
+    }
+
+
+    public void OnClick()
+    {
+
+
+        for (var i= 0; i< 5; i++ )
+        {
+            //GameObject playerCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
+            //playercard.getcomponent<image>().color = colors[Random.Range(0, colors.Count)];
+
+            GameObject playerCard = Instantiate(cards[0], new Vector3(0, 0, 0), Quaternion.identity);
+            playerCard.GetComponent<Image>().color = new Color32(32, 21, 236, 255);
+            playerCard.transform.SetParent(PlayerArea.transform, false);
+
+            GameObject enemyCard = Instantiate(Card, new Vector3(0, 0, 0), Quaternion.identity);
+            enemyCard.GetComponent<Image>().color = new Color32(236, 21, 34, 255);
+            enemyCard.transform.SetParent(EnemyArea.transform, false);
+
+            // create a tag to differentiate between player and enemy
+        }
+        
+    }
+}
